9a06faaea274c79b2a3fee34d9c356816653b281
Norbert Joo <wi19b074@technikum-wien.at>
2020-11-05 10:42:43 +0100

    Add all files

---
+++ REQ_Exam/SpecFlowFeature1.feature
@@ -0,0 +1,25 @@
+﻿Feature: Taschenrechner
+	Als Benutzer möchte ich einen Taschenrechner verwenden, 
+	damit ich trigonometrische Funktionen berechnen kann.
+
+
+@mytag
+Scenario: Calculate Sinus
+	Given the option is <sinus>
+	And the parameter is 3.14
+	When the calculate button is pressed
+	Then the result should be 0
+
+@mytag
+Scenario: Calculate Cosinus
+	Given the option is <cosinus>
+	And the parameter is 3.14
+	When the calculate button is pressed
+	Then the result should be -1
+
+@mytag
+Scenario: Calculate Tangens
+	Given the option is <tangens>
+	And the parameter is 3.14
+	When the calculate button is pressed
+	Then the result should be 0
---
+++ REQ_Exam/TaschenrechnerSteps.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace REQ_Exam
+{
+    [Binding]
+    public class TaschenrechnerSteps
+    {
+        string option;
+        Double digit;
+        Double result;
+
+        [Given(@"the option is (.*)")]
+        public void GivenTheOptionIs(string p0)
+        {
+            option = p0;
+        }
+        
+        [Given(@"the parameter is (.*)")]
+        public void GivenTheParameterIs(Double p0)
+        {
+            digit = p0;
+        }
+        
+        [When(@"the calculate button is pressed")]
+        public void WhenTheCalculateButtonIsPressed()
+        {
+            if (option == "sinus")
+            {
+                result = Math.Sin(Math.PI);
+            }else if (option == "tangens")
+            {
+                result = Math.Tan(Math.PI);
+            }else if (option == "cosinus")
+            {
+                result = Math.Cos(Math.PI);
+            }
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            if (option == "sinus")
+            {
+                Assert.Equal(p0, result, 5);
+            }
+            else if (option == "tangens")
+            {
+                Assert.Equal(p0, result, 5);
+            }
+            else if (option == "cosinus")
+            {
+                Assert.Equal(p0, result, 5);
+            }
+        }
+    }
+}
